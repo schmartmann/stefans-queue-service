8eda6c7eee9d353c6ffb9bf1161d6e2b337a08d6
schmartmann <stefanhartmann@gmail.com>
2019-01-21 10:03:40 -0500

    add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'queues#index'
+  devise_for :users,
+             path: '',
+             path_names: {
+               sign_in: 'login',
+               sign_out: 'logout',
+               registration: 'signup'
+             },
+             controllers: {
+               sessions: 'sessions',
+               registrations: 'registrations'
+             }
 end
