d5f5a49d6dd77c8d79173a4d2af2bc04a30bb760
schmartmann <stefanhartmann@gmail.com>
2019-01-21 21:05:14 -0500

    refactor test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
+RSpec.describe 'POST /login', type: :request do
   let( :user ) { Fabricate( :user ) }
   let( :url ) { '/login' }
   let( :params ) do
@@ -14,20 +14,21 @@ RSpec.describe "Users", type: :request do
 
   context 'when params are correct' do
     before do
+      binding.pry
       post url, params: params
     end
 
     it 'returns 200' do
-      expect(response).to have_http_status(200)
+      expect( response ).to have_http_status( 200 )
     end
 
     it 'returns JTW token in authorization header' do
-      expect(response.headers['Authorization']).to be_present
+      expect( response.headers[ 'Authorization' ] ).to be_present
     end
 
     it 'returns valid JWT token' do
-      decoded_token = decoded_jwt_token_from_response(response)
-      expect(decoded_token.first['sub']).to be_present
+      decoded_token = decoded_jwt_token_from_response( response )
+      expect( decoded_token.first[ 'sub' ] ).to be_present
     end
   end
 
@@ -35,7 +36,16 @@ RSpec.describe "Users", type: :request do
     before { post url }
 
     it 'returns unathorized status' do
-      expect(response.status).to eq 401
+      expect( response.status ).to eq 401
     end
+  end
+end
+
+RSpec.describe 'DELETE /logout', type: :request do
+  let( :url ) { '/logout' }
+
+  it 'returns 204, no content' do
+    delete url
+    expect( response ).to have_http_status( 204 )
   end
 end
