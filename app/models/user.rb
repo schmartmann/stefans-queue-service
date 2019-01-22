b2ef45a79b48ff0eee9d7f6e29cb4a388d96002e
schmartmann <stefanhartmann@gmail.com>
2019-01-22 11:32:01 -0500

    user model validates password

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,8 @@ class User < ApplicationRecord
   validates :email,
             presence: true,
             uniqueness: true
+
+  validates :password,
+            presence: true,
+            length: { minimum: 8 }
 end
