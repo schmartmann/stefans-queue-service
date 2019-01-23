637ed7a1e85a04de5b986efd9d271c3de704fd2b
schmartmann <stefanhartmann@gmail.com>
2019-01-23 17:36:25 -0500

    rebase

--- spec/fabricators/policy_fabricator.rb
+++ spec/fabricators/policy_fabricator.rb
@@ -5,14 +5,3 @@ end
 
 Fabricator( :policy_base, from: :policy ) do
 end
-
-
-Fabricator(:widget) do
-  wockets(count: 5, fabricator: :wocket_base)
-end
-
-Fabricator(:wocket_base)
-
-Fabricator(:wocket, from: :wocket) do
-  widget
-end
