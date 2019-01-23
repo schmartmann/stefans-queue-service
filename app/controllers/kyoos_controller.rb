6ad8857f3618e0f04b2015b09de2eb595a213540
schmartmann <stefanhartmann@gmail.com>
2019-01-23 14:25:13 -0500

    kyoos controller deletes

--- app/controllers/kyoos_controller.rb
+++ app/controllers/kyoos_controller.rb
@@ -6,7 +6,7 @@ class KyoosController < ApplicationController
     id
     uuid
     name
-  )
+  ).freeze
 
   def query
     render json: kyoos
@@ -17,6 +17,30 @@ class KyoosController < ApplicationController
   end
 
   def write
+    unless existing_kyoo
+      kyoo = current_user.kyoos.new( kyoo_params )
+
+      kyoo.save
+      render_resource( kyoo )
+    else
+      render_resource( existing_kyoo )
+    end
+  end
+
+  def destroy
+    kyoo = Kyoo.find_by( uuid: uuid )
+
+    unless kyoo.nil?
+      if kyoo.destroy
+        render json: {
+            message: "Kyoo #{ uuid } successfully deleted"
+          }
+      else
+        fatal_error
+      end
+    else
+      missing_resource( Exception.new( "Could not retrieve kyoo #{ uuid }" ) )
+    end
   end
 
   private
@@ -26,7 +50,15 @@ class KyoosController < ApplicationController
   end
 
   def kyoo
-    @kyoo ||= current_user.kyoos.where( params[ :uuid  ] )
+    @kyoo ||= current_user.kyoos.where( uuid: uuid )
+  end
+
+  def uuid
+    @uuid ||= params[ :uuid ]
+  end
+
+  def existing_kyoo
+    @existing_kyoo ||= kyoos.where( kyoo_params ).first
   end
 
   def kyoo_params
