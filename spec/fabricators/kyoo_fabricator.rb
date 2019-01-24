dd94d04892993622404164d5c4bc72f1b3a55a88
schmartmann <stefanhartmann@gmail.com>
2019-01-23 23:58:07 -0500

    refactor kyoos tests

--- spec/fabricators/kyoo_fabricator.rb
+++ spec/fabricators/kyoo_fabricator.rb
@@ -1,4 +1,9 @@
 Fabricator( :kyoo ) do
   uuid { SecureRandom.hex }
   name { Faker::TheThickOfIt.department }
+
+  after_create do | kyoo |
+    @user = Fabricate( :user )
+    @policy = Fabricate( :policy, user: @user, kyoo: kyoo )
+  end
 end
--- spec/requests/kyoos_request.rb
+++ spec/requests/kyoos_request.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'GET /kyoos', type: :request do
-  let( :user )   { Fabricate( :user ) }
   let( :kyoo )   { Fabricate( :kyoo ) }
   let( :url )    { '/kyoos' }
 
   context 'authenticated' do
     context 'when params are correct' do
       before do
-        Fabricate( :policy, user: user, kyoo: kyoo )
+        user = kyoo.users.first
 
         get url, headers: auth_headers( user )
       end
@@ -36,13 +35,12 @@ end
 
 RSpec.describe 'GET /kyoos/:uuid', type: :request do
   let( :kyoo )   { Fabricate(:kyoo ) }
-  let( :user )   { Fabricate( :user ) }
   let( :uuid )   { kyoo.uuid }
   let( :url )    { "/kyoos/#{ uuid }" }
 
   context 'when params are correct' do
     before do
-      Fabricate( :policy, user: user, kyoo: kyoo )
+      user = kyoo.users.first
 
       get url, headers: auth_headers( user )
     end
@@ -58,7 +56,6 @@ RSpec.describe 'GET /kyoos/:uuid', type: :request do
 end
 
 RSpec.describe 'POST /kyoos', type: :request do
-  let( :user )  { Fabricate( :user ) }
   let( :url )   { '/kyoos' }
   let( :params ) do
     {
@@ -70,6 +67,8 @@ RSpec.describe 'POST /kyoos', type: :request do
 
   context 'when params are correct' do
     before do
+      user = Fabricate( :user )
+
       post url, params: params, headers: auth_headers( user )
     end
 
@@ -84,6 +83,8 @@ RSpec.describe 'POST /kyoos', type: :request do
 
   context 'when name is missing' do
     before do
+      user = Fabricate( :user )
+
       kyoo = {
         kyoo: {
           name: nil
@@ -114,7 +115,9 @@ RSpec.describe 'POST /kyoos', type: :request do
     before do
       already_taken_name = JSON.parse( params )[ "kyoo" ][ "name" ]
 
-      Fabricate( :kyoo, name: already_taken_name )
+      kyoo = Fabricate( :kyoo, name: already_taken_name )
+
+      user = kyoo.users.first
 
       post url, params: params, headers: auth_headers( user )
     end
