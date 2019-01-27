af5d574c0a79669304f11adeaf88096a084915ab
schmartmann <stefanhartmann@gmail.com>
2019-01-26 22:52:44 -0500

    add dispatcher class

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,8 @@ module StefansQueueService
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
